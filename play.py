d1af56e55faa309708ad70279e4c6f712290d894
Jonathan Gardner <jgardner@jonathangardner.net>
2011-07-20 17:17:54 -0700

    Now you have exits.

--- play.py
+++ play.py
@@ -4,16 +4,28 @@ class Room(object):
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.exits = dict()
 
     def look(self):
         print self.name
         print self.description
 
-current_room = Room(
+room1 = Room(
     name="This room is this room",
     description="This room is a room, like other rooms but a little \
 different."
 )
+
+room2 = Room(
+    name="This is another room",
+    description="This room is slightly different from the previous room."
+)
+
+room1.exits['north'] = room2
+room2.exits['south'] = room1
+
+current_room = room1
+
 while True:
     text = raw_input("Your command? ")
 
@@ -22,6 +34,10 @@ while True:
 
     elif text == "look":
         current_room.look()
+
+    elif text in current_room.exits:
+        print "You go {}.".format(text)
+        current_room = current_room.exits[text]
 
     else:
         print "What was that?"
